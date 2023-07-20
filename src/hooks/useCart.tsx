cc59258de3a5c112ea56f65d791087bac0a38e17
Augenb1ick <augenblick14@yandex.ru>
2023-07-20 18:09:23 +0400

    minor fixes

--- src/hooks/useCart.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import CartContext from "../context/CardProvider";
-import { UseCartContextType } from "../context/CardProvider";
-
-const useCart = (): UseCartContextType => {
-    return useContext(CartContext)
-}
-
-export default useCart;
